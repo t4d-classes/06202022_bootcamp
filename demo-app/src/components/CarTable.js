a573f642ca2d37c88f0066760875a4f48d4b8ae1
Eric Greene <eric@t4d.io>
2022-06-23 17:19:04 -0400

    add solution to exercise 9

--- demo-app/src/components/CarTable.js
+++ demo-app/src/components/CarTable.js
@@ -1,25 +1,47 @@
 import { CarEditRow } from './CarEditRow';
 import { CarViewRow } from './CarViewRow';
 
+
+const ColSortHeader = ({
+  headerText, fieldName, sortCol, sortDir, onSortCars: sortCars
+}) => {
+  return (
+    <th>
+      <button type="button" onClick={() => sortCars(fieldName)}>
+        {headerText} {sortCol === fieldName && <span>({sortDir})</span>}
+      </button>
+    </th>    
+  );
+};
+
 export const CarTable = ({
   cars,
   editCarId,
+  sortCol,
+  sortDir,
+  onSortCars,
   onEditCar: editCar,
   onDeleteCar: deleteCar,
   onSaveCar: saveCar,
   onCancelCar: cancelCar,
 }) => {
 
+  const colSortHeaders = [
+    { headerText: "Id", fieldName: "id" },
+    { headerText: "Make", fieldName: "make" },
+    { headerText: "Model", fieldName: "model" },
+    { headerText: "Year", fieldName: "year" },
+    { headerText: "Color", fieldName: "color" },
+    { headerText: "Price", fieldName: "price" },
+  ];
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Id</th>
-          <th>Make</th>
-          <th>Model</th>
-          <th>Year</th>
-          <th>Color</th>
-          <th>Price</th>
+          {colSortHeaders.map(csh =>
+            <ColSortHeader key={csh.fieldName} {...csh}
+              {...{sortCol, sortDir, onSortCars}} />)}            
           <th>Actions</th>
         </tr>
       </thead>        
--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -4,11 +4,31 @@ import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
 import { CarForm } from './CarForm';
 
+const SORT_DIR_ASC = 'asc';
+const SORT_DIR_DESC = 'desc';
+
+
+const sortTheCars = (cars, sortCol, sortDir) => {
+
+  return [...cars].sort((a,b) => {
+    if (a[sortCol] < b[sortCol]) {
+      return sortDir === SORT_DIR_ASC ? -1 : 1
+    } else if (a[sortCol] > b[sortCol]) {
+      return sortDir === SORT_DIR_ASC ? 1 : -1
+    } else {
+      return 0;
+    }
+  });
+};
+
 export const CarTool = ({ cars: initialCars }) => {
 
   const [ cars, setCars ] = useState([...initialCars]);
 
   const [ editCarId, setEditCarId ] = useState(-1);
+
+  const [ sortCol, setSortCol ] = useState('id');
+  const [ sortDir, setSortDir ] = useState(SORT_DIR_ASC);
 
   const editCar = carId => {
     setEditCarId(carId);
@@ -42,11 +62,21 @@ export const CarTool = ({ cars: initialCars }) => {
     setEditCarId(-1);
   };
 
+  const sortCars = (colName) => {
+    if (colName === sortCol) {
+      setSortDir(sortDir === SORT_DIR_ASC ? SORT_DIR_DESC : SORT_DIR_ASC);
+    } else {
+      setSortCol(colName);
+      setSortDir(SORT_DIR_ASC);
+    }
+  };
+
   return (
     <>
       <ToolHeader headerText="Car Tool" />
-      <CarTable cars={cars} editCarId={editCarId}
-        onEditCar={editCar} onDeleteCar={deleteCar}
+      <CarTable cars={sortTheCars(cars, sortCol, sortDir)}
+        sortCol={sortCol} sortDir={sortDir} editCarId={editCarId}
+        onSortCars={sortCars} onEditCar={editCar} onDeleteCar={deleteCar}
         onSaveCar={saveCar} onCancelCar={cancelCar} />
       <CarForm buttonText="Add Car" onSubmitCar={addCar} />
     </>
